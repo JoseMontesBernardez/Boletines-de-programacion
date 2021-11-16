0c19641ed388a013128a0fc2f4e4d85604db0acc
Jose Montes Bernardez <jmontesbernardez@danielcastelao.org>
2021-11-16 11:00:41 +0100

    commit

---
+++ boletin11_2/src/boletin11_2/Juego_2.java
@@ -0,0 +1,48 @@
+package boletin11_2;
+
+import javax.swing.*;
+
+public class Juego_2 {
+    public void adivinarNumero () {
+        int r_1 = 1;
+        int r_2 = 50;
+        int i = numeroIntentos();
+        int j;
+        int n_1 = (int) (Math.random()*r_2+1);
+
+        JOptionPane.showMessageDialog(null, "El numero estará entre " + r_1 + " y " + r_2+" y tienes "+i+" intentos");
+        int n_2;
+
+        for (j=1; j<=i; j++){
+            n_2 = pedirNumero();
+            if (n_2 == n_1){
+                JOptionPane.showMessageDialog(null, "HAS GANADO");
+                break;
+
+            }else if (Math.abs(n_2 - n_1) > 20 ){
+                JOptionPane.showMessageDialog(null, "Estás muy lejos");
+            }else if (Math.abs(n_2 - n_1) >= 10) {
+                JOptionPane.showMessageDialog(null, "Estás lejos");
+            }else if (Math.abs(n_2 - n_1) >= 5){
+                JOptionPane.showMessageDialog(null, "Estás cerca");
+            }else if (Math.abs(n_2-n_1) < 5){
+                JOptionPane.showMessageDialog(null, "Estás muy cerca");
+            }
+        }
+        JOptionPane.showMessageDialog(null, "HAS PERDIDO");
+
+    }
+
+
+    public int pedirNumero(){
+        int num_2 = Integer.parseInt(JOptionPane.showInputDialog("Adivina el numero: "));
+        return num_2;
+    }
+
+
+    public int numeroIntentos(){
+        int intentos = Integer.parseInt(JOptionPane.showInputDialog("Numero de intentos del jugador"));
+        return  intentos;
+    }
+
+}
---
+++ boletin11_2/src/boletin11_2/boletin11_2.java
@@ -0,0 +1,8 @@
+package boletin11_2;
+
+public class boletin11_2 {
+    public static void main(String[] args) {
+        Juego_2 obx = new Juego_2();
+        obx.adivinarNumero();
+    }
+}
