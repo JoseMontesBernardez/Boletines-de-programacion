9f6d59bb409446703a4fb55a97bf13ac3eacbf1f
Jose Montes Bernardez <jmontesbernardez@danielcastelao.org>
2021-12-14 10:05:04 +0100

    commit

---
+++ 1EVA/boletin2_1/src/boletin2_1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class boletin2_1 {
+
+    public static void main(String[] args) {
+        int base = 4;
+        int altura = 3;
+        float resultado = (base*altura)/2;
+
+        JOptionPane.showMessageDialog(null, "El area del triangulo es:" +resultado);
+    }
+}
