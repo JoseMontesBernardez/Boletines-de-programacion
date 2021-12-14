850e59135169b6ec82dc5bf4f798ce9f2e8c9f93
Jose Montes Bernardez <jmontesbernardez@danielcastelao.org>
2021-12-14 10:09:41 +0100

    commit

---
+++ 1EVA/boletin2_2/src/boletin2_2.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class boletin2_2 {
+    public static void main(String[] args) {
+        int lado = 3;
+        int area = lado*lado;
+
+        JOptionPane.showMessageDialog(null, "El area del cuadrado es: "+area);
+    }
+}
